23d4c974a5d2cc0ca38901ab55dafee1a2e91f09
linh222 <Tranquanglinh222@gmail.com>
2023-09-06 13:23:16 +0100

    add lavis directory

---
+++ LAVIS/lavis/processors/__init__.py
@@ -0,0 +1,53 @@
+"""
+ Copyright (c) 2022, salesforce.com, inc.
+ All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+ For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
+from LAVIS.lavis.processors.base_processor import BaseProcessor
+
+from LAVIS.lavis.processors.alpro_processors import (
+    AlproVideoTrainProcessor,
+    AlproVideoEvalProcessor,
+)
+from LAVIS.lavis.processors.blip_processors import (
+    BlipImageTrainProcessor,
+    Blip2ImageTrainProcessor,
+    BlipImageEvalProcessor,
+    BlipCaptionProcessor,
+)
+from LAVIS.lavis.processors.gpt_processors import (
+    GPTVideoFeatureProcessor,
+    GPTDialogueProcessor,
+)
+from LAVIS.lavis.processors.clip_processors import ClipImageTrainProcessor
+
+from LAVIS.lavis.common.registry import registry
+
+__all__ = [
+    "BaseProcessor",
+    # ALPRO
+    "AlproVideoTrainProcessor",
+    "AlproVideoEvalProcessor",
+    # BLIP
+    "BlipImageTrainProcessor",
+    "Blip2ImageTrainProcessor",
+    "BlipImageEvalProcessor",
+    "BlipCaptionProcessor",
+    "ClipImageTrainProcessor",
+    # GPT
+    "GPTVideoFeatureProcessor",
+    "GPTDialogueProcessor",
+]
+
+
+def load_processor(name, cfg=None):
+    """
+    Example
+
+    >>> processor = load_processor("alpro_video_train", cfg=None)
+    """
+    processor = registry.get_processor_class(name).from_config(cfg)
+
+    return processor
