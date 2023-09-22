722f2906feeacf8220e4e8df98643706220f59ec
linh222 <Tranquanglinh222@gmail.com>
2023-09-22 11:01:12 +0100

    code to ingest data by row

--- app/ingest_data.py
+++ app/ingest_data.py
@@ -1,12 +1,12 @@
+from io import StringIO
 from os.path import join, dirname
 
 import pandas as pd
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
-from tqdm import tqdm
 
-from config import HOST, root_path
+from config import HOST, INDICES
+from utils import create_index, index_data2elasticsearch
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -16,35 +16,29 @@ schema = {
     "mappings": {
         "properties": {
             "ImageID": {"type": "text"},
+            "Tags": {"type": "text"},
+            "ORC": {"type": "text"},
+            "Caption": {"type": "text"},
             "new_name": {"type": "text"},
             "city": {"type": "text"},
             "event_id": {"type": "text"},
             "local_time": {"type": "date"},
             "semantic_name": {"type": "text"},
+            "hour": {"type": "integer"},
             "date_of_week": {"type": "text"},
+            "is_weekend": {"type": "integer"},
             "time_period": {"type": "text"},
-            "visual_concept": {"type": "text"},
             "blip_embed": {"type": "dense_vector", "dims": 256,
                            "index": True, "similarity": "cosine"
                            }
         }
     }
 }
-es.indices.create(index='lsc23', body=schema)
+result = create_index(es=es, HOST=HOST, indice=INDICES, schema=schema)
+response = s3.get_object(Bucket=BUCKET, Key='grouped_info_dict_full_blip2.json')
+json_data = response['Body'].read()
+df = pd.read_json(
+    StringIO(json_data),
+    orient='index')
 
-df = pd.read_json('{}/elasticsearch-data/data_4_ingest_visualconcept.json'.format(root_path), orient='index')
-df['index'] = df['ImageID']
-
-
-def gen_data():
-    for i, row in tqdm(df.iterrows(), total=len(df)):
-        data = row.to_dict()
-        data.pop('index', None)
-        yield {
-            "_index": 'lsc23',
-            "_id": row['index'],
-            "_source": data
-        }
-
-
-bulk(es, gen_data())
+result = index_data2elasticsearch(df=df, indice=INDICES, host=HOST)
