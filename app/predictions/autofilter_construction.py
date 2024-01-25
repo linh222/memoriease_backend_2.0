21837031e6e98c239d68a5c9325fa1adc7283013
linh222 <Tranquanglinh222@gmail.com>
2024-01-25 15:21:48 +0700

    update auto filter construction pront

--- app/predictions/autofilter_construction.py
+++ app/predictions/autofilter_construction.py
@@ -3,6 +3,7 @@ import os
 import pickle
 
 import openai
+from openai import OpenAI
 import requests
 from dotenv import load_dotenv
 
@@ -31,39 +32,145 @@ def send_gpt_request(query):
     # TODO Prompt engineering to improve the accuracy
     # TODO Iterate until chatgpt give the correct format
 
-    prompt = """Query: Having lunch with Dermot, who was a guest speaker at my lecture. After lunch, he gave a 
-    lecture to my class about Lessons in Innovation & Entrepreneurship while I was sitting in the front row. It was 
-    in November 2019. Result: [{'term': {'concept': 'Having lunch with Dermot, who was a guest speaker at my 
-    lecture'}},{'term': {'after_concept': 'After lunch, he gave a lecture to my class about Lessons in Innovation & 
-    Entrepreneurship in dublin city university while I was sitting in the front row.'}},{'term': {'before_concept': 
-    ''}},{'term': {'time_period': ''}}, {'term': {'day_of_week': ''}}, {'range': {'local_time': {'gte': '2019-11-01', 
-    'lte': '2019-11-30'}}}, {'term': {'city': 'dublin'}}] Query: Drinks on top of the Bangkok. Taking a drink on a 
-    rooftop bar at night in Bangkok. It was on the same day that I flew into Bangkok. In 2019 in September. Result: [
-    {'term': {'concept': 'Drinks on top of the Bangkok. Taking a drink on a rooftop bar at night in Bangkok.'}}, 
-    {'term': {'after_concept': ''}}, {'term': {'before_concept': 'It was on the same day that I flew into Bangkok'}}, 
-    {'term': {'time_period': 'night'}}, {'term': {'day_of_week': ''}}, {'range': {'local_time': {'gte': '2019-09-01', 
-    'lte': '2019-09-30'}}}, {'term': {'city': 'bangkok'}}] Query: I remember that there was a man in a pink t-shirt 
-    in front of a wall looking at the water. Result: [{'term': {'concept': 'there was a man in a pink t-shirt in 
-    front of a wall looking at the water.'}}, {'term': {'after_concept': ''}}, {'term': {'before_concept': ''}}, 
-    {'term': {'time_period': ''}}, {'term': {'day_of_week': ''}}, {'range': {'local_time': {'gte': '2019-01-01', 
-    'lte': '2020-06-30'}}}, {'term': {'city': ''}}] Query: I was getting an eye test after looking for a new pair of 
-    glasses. I remember the optician had a red sweater with a reindeer on it and that I had to look into some 
-    machines. After the eye test I went shopping for groceries. It was a few days before Christmas. Result: [{'term': 
-    {'concept': 'I was getting an eye test after looking for a new pair of glasses. I remember the optician had a red 
-    sweater with a reindeer on it and that I had to look into some machines'}}, {'term': {'after_concept': 'Shopping 
-    for groceries'}}, {'term': {'before_concept': ''}}, {'term': {'time_period': ''}}, {'term': {'day_of_week': ''}}, 
-    {'range': {'local_time': {'gte': '2019-12-01', 'lte': '2019-12-24'}}}, {'term': {'city': ''}}]"""
-    query_format = f"Query: {query}\n Result: "
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct-0914",
-        prompt=prompt + query_format,
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful query extractor to extract relevant information from a query to form a "
+                           "json file to query in elastic search. The set of valid values for time_perios is ["
+                           "'morning', 'afternoon', 'night', 'late night', 'early morning'] and for day_of_week is ["
+                           "'monday', 'tuesday', 'webnesday', 'thursday', 'friday', 'saturday', 'sunday'] and city is "
+                           "only the name of the city\nHere are some examples of the task:\nPrompt examples for "
+                           "filter construction.\nExtract the information from query to the result as follow: "
+                           "\nQuery: Having lunch with Dermot, who was a guest speaker at my lecture. After lunch, "
+                           "he gave a lecture to my class about Lessons in Innovation & Entrepreneurship while I was "
+                           "sitting in the front row. It was in November 2019.\nResult: [{'term': {'concept': 'Having "
+                           "lunch with Dermot, who was a guest speaker at my lecture'}}, {'term': {'after_concept': "
+                           "'After lunch, he gave a lecture to my class about Lessons in Innovation & "
+                           "Entrepreneurship in Dublin City University while I was sitting in the front row.'}},"
+                           "{'term': {'before_concept':''}},{'term': {'time_period': 'afternoon'}}, {'term': {"
+                           "'day_of_week': ''}}, {'range': {'local_time': {'gte': '2019-11-01',"
+                           "\n'lte': '2019-11-30'}}}, {'term': {'city': 'dublin'}}]\nI was getting an eye test after "
+                           "looking for a new pair of glasses. I remember the optician had a red sweater with a "
+                           "reindeer on it and that I had to look into some machines. After the eye test, "
+                           "I went shopping for groceries. It was a few days before Christmas.\nResult: [{'term': {"
+                           "'concept': 'I was getting an eye test. I remember the optician had a red sweater with a "
+                           "reindeer on it and that I had to look into some machines'}}, {'term': {'after_concept': "
+                           "'Shopping for groceries'}}, {'term': {'before_concept': 'looking for a new pair of "
+                           "glasses'}}, {'term': {'time_period': ''}}, {'term': {'day_of_week': ''}},\n{'range': {"
+                           "'local_time': {'gte': '2019-12-15', 'lte': '2019-12-24'}}}, {'term': {'city': ''}}]\nWhen "
+                           "did I buy that model train? I remember it was a marklin brand train and I bought it at "
+                           "the weekend. Jer convinced me to buy it when having coffee and I bought it immediately "
+                           "after coffee. It was in June 2019.\nResult: [{'term': {'concept': 'When did I buy that "
+                           "model train? I remember it was a marklin brand train'}}, {'term': {'after_concept': ''}}, "
+                           "{'term': {'before_concept': 'Jer convinced me to buy it when having coffee'}}, "
+                           "{'term': {'time_period': ''}}, {'term': {'day_of_week': ''}}, {'range': {'local_time': {"
+                           "'gte': '2019-06-01', 'lte': '2019-06-30'}}}, {'term': {'city': ''}}]\nQuery: I remember a "
+                           "man in a blue coat walking a dog in the countryside in Ireland on a sunny afternoon in "
+                           "December on Christmas Day.\n-> Result: [{'term': {'concept': 'a man in a blue coat "
+                           "walking a dog in the countryside on a sunny afternoon'}}, {'term': {'after_concept': "
+                           "''}},\n{'term': {'before_concept': ''}}, {'term': {'time_period': 'afternoon'}}, "
+                           "{'term': {'day_of_week': ''}},\n{'range': {'local_time': {'gte': '2019-12-25', "
+                           "'lte': '2019-12-25'}}}, {'term': {'city': 'Ireland'}}]\nQuery: I remember that there was "
+                           "a man in a pink t-shirt in front of a wall looking at the water.\n-> Result: [{'term': {"
+                           "'concept': 'there was a man in a pink t-shirt in front of a wall looking at the "
+                           "water.'}},\n{'term': {'after_concept': ''}}, {'term': {'before_concept': ''}}, "
+                           "{'term': {'time_period': ''}}, {'term': {'day_of_week': ''}},\n{'range': {'local_time': {"
+                           "'gte': '2019-01-01', 'lte': '2020-06-30'}}}, {'term': {'city': ''}}]\nQuery: Find "
+                           "examples of when I was eating avocado for breakfast\n-> Result: [{'term': {'concept': 'I "
+                           "was eating avocado for breakfast'}},\n{'term': {'after_concept': ''}}, {'term': {"
+                           "'before_concept': ''}}, {'term': {'time_period': 'morning'}}, {'term': {'day_of_week': "
+                           "''}},\n{'range': {'local_time': {'gte': '2019-01-01', 'lte': '2020-06-30'}}}, "
+                           "{'term': {'city': ''}}]\nQuery: Damn it, my car has a flat tyre. What was the name of the "
+                           "car service/repair company that I used in the summer of 2019? I want to call them to get "
+                           "my car fixed.\n-> Result: [{'term': {'concept': 'Damn it, my car has a flat tyre. The "
+                           "name of the car service/repair company that I used to get my car fixed.'}},\n{'term': {"
+                           "'after_concept': ''}}, {'term': {'before_concept': ''}}, {'term': {'time_period': "
+                           "'morning'}}, {'term': {'day_of_week': ''}},\n{'range': {'local_time': {'gte': "
+                           "'2019-04-01', 'lte': '2019-08-31'}}}, {'term': {'city': ''}}]\nQuery: Trying on clothes. "
+                           "Find images of me trying on clothes in a changing room in a clothing store.\n-> "
+                           "Result:Prompt examples for filter construction.\nExtract the information from query to "
+                           "the result as follow: \nQuery: Having lunch with Dermot, who was a guest speaker at my "
+                           "lecture. After lunch, he gave a lecture to my class about Lessons in Innovation & "
+                           "Entrepreneurship while I was sitting in the front row. It was in November 2019.\nResult: "
+                           "[{'term': {'concept': 'Having lunch with Dermot, who was a guest speaker at my "
+                           "lecture'}}, {'term': {'after_concept': 'After lunch, he gave a lecture to my class about "
+                           "Lessons in Innovation & Entrepreneurship in Dublin City University while I was sitting in "
+                           "the front row.'}},{'term': {'before_concept':''}},{'term': {'time_period': 'afternoon'}}, "
+                           "{'term': {'day_of_week': ''}}, {'range': {'local_time': {'gte': '2019-11-01',"
+                           "\n'lte': '2019-11-30'}}}, {'term': {'city': 'dublin'}}]\nI was getting an eye test after "
+                           "looking for a new pair of glasses. I remember the optician had a red sweater with a "
+                           "reindeer on it and that I had to look into some machines. After the eye test, "
+                           "I went shopping for groceries. It was a few days before Christmas.\nResult: [{'term': {"
+                           "'concept': 'I was getting an eye test. I remember the optician had a red sweater with a "
+                           "reindeer on it and that I had to look into some machines'}}, {'term': {'after_concept': "
+                           "'Shopping for groceries'}}, {'term': {'before_concept': 'looking for a new pair of "
+                           "glasses'}}, {'term': {'time_period': ''}}, {'term': {'day_of_week': ''}},\n{'range': {"
+                           "'local_time': {'gte': '2019-12-15', 'lte': '2019-12-24'}}}, {'term': {'city': ''}}]\nWhen "
+                           "did I buy that model train? I remember it was a marklin brand train and I bought it at "
+                           "the weekend. Jer convinced me to buy it when having coffee and I bought it immediately "
+                           "after coffee. It was in June 2019.\nResult: [{'term': {'concept': 'When did I buy that "
+                           "model train? I remember it was a marklin brand train'}}, {'term': {'after_concept': ''}}, "
+                           "{'term': {'before_concept': 'Jer convinced me to buy it when having coffee'}}, "
+                           "{'term': {'time_period': ''}}, {'term': {'day_of_week': ''}}, {'range': {'local_time': {"
+                           "'gte': '2019-06-01', 'lte': '2019-06-30'}}}, {'term': {'city': ''}}]\nQuery: I remember a "
+                           "man in a blue coat walking a dog in the countryside in Ireland on a sunny afternoon in "
+                           "December on Christmas Day.\n-> Result: [{'term': {'concept': 'a man in a blue coat "
+                           "walking a dog in the countryside on a sunny afternoon'}}, {'term': {'after_concept': "
+                           "''}},\n{'term': {'before_concept': ''}}, {'term': {'time_period': 'afternoon'}}, "
+                           "{'term': {'day_of_week': ''}},\n{'range': {'local_time': {'gte': '2019-12-25', "
+                           "'lte': '2019-12-25'}}}, {'term': {'city': 'Ireland'}}]\nQuery: I remember that there was "
+                           "a man in a pink t-shirt in front of a wall looking at the water.\n-> Result: [{'term': {"
+                           "'concept': 'there was a man in a pink t-shirt in front of a wall looking at the "
+                           "water.'}},\n{'term': {'after_concept': ''}}, {'term': {'before_concept': ''}}, "
+                           "{'term': {'time_period': ''}}, {'term': {'day_of_week': ''}},\n{'range': {'local_time': {"
+                           "'gte': '2019-01-01', 'lte': '2020-06-30'}}}, {'term': {'city': ''}}]\nQuery: Find "
+                           "examples of when I was eating avocado for breakfast\n-> Result: [{'term': {'concept': 'I "
+                           "was eating avocado for breakfast'}},\n{'term': {'after_concept': ''}}, {'term': {"
+                           "'before_concept': ''}}, {'term': {'time_period': 'morning'}}, {'term': {'day_of_week': "
+                           "''}},\n{'range': {'local_time': {'gte': '2019-01-01', 'lte': '2020-06-30'}}}, "
+                           "{'term': {'city': ''}}]\nQuery: Damn it, my car has a flat tyre. What was the name of the "
+                           "car service/repair company that I used in the summer of 2019? I want to call them to get "
+                           "my car fixed.\n-> Result: [{'term': {'concept': 'Damn it, my car has a flat tyre. The "
+                           "name of the car service/repair company that I used to get my car fixed.'}},\n{'term': {"
+                           "'after_concept': ''}}, {'term': {'before_concept': ''}}, {'term': {'time_period': "
+                           "'morning'}}, {'term': {'day_of_week': ''}},\n{'range': {'local_time': {'gte': "
+                           "'2019-04-01', 'lte': '2019-08-31'}}}, {'term': {'city': ''}}]\nQuery: Trying on clothes. "
+                           "Find images of me trying on clothes in a changing room in a clothing store.\n-> Result: ["
+                           "{'term': {'concept': 'Trying on clothes.'}},\n{'term': {'after_concept': ''}}, "
+                           "{'term': {'before_concept': 'in a clothing store'}}, {'term': {'time_period': ''}}, "
+                           "{'term': {'day_of_week': ''}},\n{'range': {'local_time': {'gte': '2019-01-01', "
+                           "'lte': '2020-06-30'}}}, {'term': {'city': ''}}]"
+            },
+            {
+                "role": "user",
+                "content": "Query: Find the image which display Mother Mary, pray for us. A religious image in front "
+                           "of a window on a Saturday morning in Dublin City University after coffee and before a "
+                           "second coffee in a different cafe."
+            },
+            {
+                "role": "assistant",
+                "content": "[{'term': {'concept': 'Mother Mary, pray for us. A religious image in front of a "
+                           "window'}},\n {'term': {'after_concept': 'a second coffee in a different cafe'}},"
+                           "\n {'term': {'before_concept': 'after coffee'}},\n {'term': {'time_period': 'morning'}},"
+                           "\n {'term': {'day_of_week': 'saturday'}},\n {'range': {'local_time': {'gte': '',"
+                           "\n    'lte': ''}}},\n {'term': {'city': 'Dublin City University'}}]"
+            },
+            {
+                "role": "user",
+                "content": f"Query: {query}"
+            }
+        ],
         temperature=1,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-    return response['choices'][0]['text']
+    return response.choices[0].message.content
 
 
 def construct_filter(query):
@@ -147,19 +254,19 @@ def retrieve_result(main_event_context: str, previous_event_context: str, after_
                                                                       main_event=event, time_gap=2,
                                                                       time_period='', location='', keyword='',
                                                                       weekday='', embed=previous_text_embedding,
-                                                                      type='previous')
+                                                                      temporal_type='previous')
                 if after_event_context != '':
                     list_next_event_query = time_processing_event(list_event_query=list_next_event_query,
                                                                   main_event=event, time_gap=2,
                                                                   time_period='', location='', keyword='',
                                                                   weekday='', embed=after_text_embedding,
-                                                                  type='next')
+                                                                  temporal_type='next')
         if previous_event_context != '':
-            new_results = send_request_by_event(new_result=new_results, event_query=list_previous_event_query,
-                                                type='previous')
+            new_results = send_request_by_event(main_event_result=new_results, event_query=list_previous_event_query,
+                                                temporal_type='previous')
         if after_event_context != '':
-            new_results = send_request_by_event(new_result=new_results, event_query=list_next_event_query,
-                                                type='next')
+            new_results = send_request_by_event(main_event_result=new_results, event_query=list_next_event_query,
+                                                temporal_type='next')
 
         new_results = calculate_overall_score(new_results)
         return new_results
